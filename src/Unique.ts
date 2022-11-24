9712d0391a96c0abde2d449fb58412916f480626
Alex Savchenko <ua.quetzalcoatl@gmail.com>
2022-11-24 12:51:28 +0000

    part 4

---
+++ src/Unique.ts
@@ -0,0 +1,45 @@
+/*
+  5360 - Unique
+  -------
+  by Pineapple (@Pineapple0919) #medium #array
+
+  ### Question
+
+  Implement the type version of Lodash.uniq, Unique<T> takes an Array T, returns the Array T without repeated values.
+
+  ```ts
+  type Res = Unique<[1, 1, 2, 2, 3, 3]>; // expected to be [1, 2, 3]
+  type Res1 = Unique<[1, 2, 3, 4, 4, 5, 6, 7]>; // expected to be [1, 2, 3, 4, 5, 6, 7]
+  type Res2 = Unique<[1, "a", 2, "b", 2, "a"]>; // expected to be [1, "a", 2, "b"]
+  type Res3 = Unique<[string, number, 1, "a", 1, string, 2, "b", 2, number]>; // expected to be [string, number, 1, "a", 2, "b"]
+  type Res4 = Unique<[unknown, unknown, any, any, never, never]>; // expected to be [unknown, any, never]
+  ```
+
+  > View on GitHub: https://tsch.js.org/5360
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type IsSame<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false
+type InArray<Arr extends any[], Val> = Arr extends []
+  ? false
+  : Arr extends [infer F, ...infer L]
+    ? (IsSame<F, Val> extends true ? true: InArray<L, Val>)
+    : false
+type Unique<T extends any[], acc extends any[] = []> = T extends []
+  ? acc
+  : T extends [infer F, ...infer L]
+    ? (Unique<L, InArray<acc, F> extends true ? acc : [...acc, F]>)
+    : never
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+]
